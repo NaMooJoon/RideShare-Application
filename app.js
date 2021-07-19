f4c6ddfe94696cde0ae5debfd8164d269c28dbab
NaMooJoon <47983023+NaMooJoon@users.noreply.github.com>
2021-07-19 15:14:59 +0900

    경식 request merge전 commit

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy
+var session = require('express-session')
+var flash = require('connect-flash')
 
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/user');
@@ -20,6 +24,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// passport setting
+app.use(session({
+  secret: 'keyboard dog',
+  resave: false,            // session이 항상 저장될지 여부를 정하는 값
+  saveUninitialized: true   // Session이 필요할 때만 구동시킨다.
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
 
 //app.use('/', indexRouter);
 app.get('/', function(req, res) {
--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET main listing. */
 router.get('/', function(req, res, next) {
-    res.render('Main');
+    console.log('main is loaded', req.user);
+    var id = req.user;
+    res.render('Main', {'id': id});
+    //res.render('Main',);
 });
 
 
