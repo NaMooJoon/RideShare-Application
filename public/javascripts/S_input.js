f6fec7ab06736e18b41bcd8907c56e945de0da44
jomakerforGod <74284597+joseph0305@users.noreply.github.com>
2021-07-21 18:40:32 +0900

    Develped S_input page
    S_input page를 개발했습니다. dropdown 를 이용해 출발지와 도착지를 입력받게 만들었습니다. Plus-minus button 을 만들어 인원 제한 수를 입력받게 했습니다. Materilze의 datepicker 와 timepicker를 가져와 날짜와 시간을 받아올수 있도록했습니다. dropdown에 여러 항목을 입력받게 하여 요일을 입력받게 했습니다. 그리고 Main page 와 javascript로 연결했습니다. S_input page에서 입력받은 데이터를 Main page에 있는 리스트에 적용시켰습니다.

---
+++ public/javascripts/S_input.js
@@ -0,0 +1,128 @@
+
+
+// 출발지 검색 드롭다운 활성화
+$('#Search-Cate-start')
+  .dropdown({
+    action: 'activate',
+  
+
+  })
+;
+
+// 도착지 검색 드롭다운 활성화
+$('#Search-Cate-end')
+  .dropdown({
+    action: 'activate',
+    
+
+  })
+;
+
+// 타임 픽커 활성화
+const timer = document.querySelector(".timepicker");
+M.Timepicker.init(timer,{});
+
+// 요일 선택 활성화
+$('#week')
+  .dropdown({
+    maxSelections: 7
+  })
+;
+
+// 날짜 픽커 활성화
+const date = document.querySelector(".datepicker");
+M.Datepicker.init(date,{});
+
+// plus-minus 활성화
+$(function(){
+    $('input[type="number"]').niceNumber();
+  });
+
+// 이용 수단 활성화
+  $('#Trans')
+  .dropdown({
+  })
+;
+
+let Sendarray = [];
+
+// Submit button 클릭시 데이터 변수화 
+$("#summitBT").click(function () {
+    let PMValue = $("#mid").val();
+    let StartLocation = $('#Search-Cate-start').dropdown('get value');
+    let EndLocation = $('#Search-Cate-end').dropdown('get value');
+    let TransValue = $('#Trans').dropdown('get value');
+    let week =$("#weekinput").val();
+    let TextArea = $("#Comments_input").val();
+    let Tinput = document.querySelector("#time");
+    let TimeInput = Tinput.value;
+    let Dinput = document.querySelector("#date");
+    let DateInput = Dinput.value;
+    let array1 = [TextArea,TimeInput,DateInput,PMValue,week,StartLocation,EndLocation,TransValue];
+    /* let Sbutton = document.querySelector("#summitBT");
+    for (let element of array1) {
+        if (element === ""){  document.querySelector("#me").innerHTML = "<span>HI</span>";  }
+        else{ }
+    } */
+    console.log(TextArea,TimeInput,DateInput,PMValue,week,StartLocation,EndLocation,TransValue)
+
+    let Send_OBJ = {
+        
+        Location_start: StartLocation,
+        Location_end:  EndLocation,
+        Start_time: TimeInput,
+        Repeat_ornot: week,
+        Start_date: DateInput,
+        Limit_person: PMValue,
+        transport_way: TransValue,
+        comments: TextArea,
+        li_id: Date.now(),
+        label_id: Date.now() * 1000,
+    }
+    Sendarray.push(Send_OBJ);
+    saveDATA();
+}); 
+
+
+
+
+// Data 화면 넘어갈때 초기화 안되도록 array update하기
+const savedupdate = JSON.parse(localStorage.getItem("Datas"));
+if (savedupdate!==null){
+    Sendarray = savedupdate;
+    
+}
+// Data를 localstorage에 저장 하기
+function saveDATA(){
+    
+    localStorage.setItem("Datas",JSON.stringify(Sendarray));
+}
+
+
+/* JSON 형식으로 줄것 */
+/* {
+    Location_start: string으로 ex) 커피유야
+    Location_end:  string으로 ex) 커피유야
+    Start_time:  00:00 PM  형식으로 ex) 04:45 AM
+    Repeat_ornot: 만약 월,화 선택시 - mon,tue / 한번실행 선택시 - Once 
+    Start_date: 월(영어 3글자) 일(숫자 2글자) 년(2021) 형식으로 ex) Jul 08
+    Limit_person: 숫자 ex) 3
+    transport_way: 영어형식으로 ex) 탑승자면 rider 버스면 bus
+    comments: string 형식으로 ex) 안녕하세요 신경식 입니다.
+}
+ */
+
+/* Limit_person: "5"
+Location_end: "하나로마트"
+Location_start: "그랜드할인마트"
+Repeat_ornot: "mon,tue"
+Start_date: "Jul 08, 2021"
+Start_time: "02:54 PM"
+comments: "안녕하세요 카풀 구해요!!"
+transport_way: "rider" */
+
+
+/* let year = $('.datepicker').pickadate('picker').get('highlight', 'yyyy');
+let day = $('.datepicker').pickadate('picker').get('highlight', 'dd');
+let month = $('.datepicker').pickadate('picker').get('highlight', 'mm');
+ console.log(year,day,month) */
