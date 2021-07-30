be6ce456adc9228318365103858d10b850cc3c6d
NaMooJoon <47983023+NaMooJoon@users.noreply.github.com>
2021-07-30 23:20:24 +0900

    Userinfo, Userlist 창 데이터 콜에대한 데이터베이스 조회

--- routes/ride-share.js
+++ routes/ride-share.js
@@ -4,18 +4,27 @@ var db = require('../lib/db');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('User_list');
+  	res.render('User_list');
 });
 
-router.get('/info/data', function(req, res, next) {
-    var query = db.connection.query('SELECT R.li_id, R.Location_start, R.Location_end, R.comments, user.stID, user.name FROM RideList AS R LEFT JOIN user ON author_id=stID WHERE R.li_id=1234', function(err, rows){
+router.get('/data', function(req, res, next) {
+    var query = db.connection.query('SELECT * FROM RideList LEFT JOIN user ON author_id=stID', function(err, rows){
+		console.log(rows);
+		if(err) throw err;
+		res.json(rows);
+	});
+});
+
+
+router.get('/:infoID/data', function(req, res, next) {
+    var query = db.connection.query('SELECT R.li_id, R.Location_start, R.Location_end, R.comments, user.stID, user.name FROM RideList AS R LEFT JOIN user ON author_id=stID WHERE R.li_id=?',[req.params.infoID], function(err, rows){
 		console.log(rows);
         if(err) throw err;
 		res.json(rows[0]);
     });
 })
 
-router.get('/info/:infoID', function(req, res, next) {
+router.get('/:infoID', function(req, res, next) {
   res.render('User_info');
 });
 
