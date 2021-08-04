e882348224da870585f2961307d51249f19c771b
yjun02 <jyjoons@gmail.com>
2021-08-04 12:36:41 +0900

    Changed background color

--- public/javascripts/User_info.js
+++ public/javascripts/User_info.js
@@ -48,5 +48,5 @@ var deg = 45;
 
 playAlert = setInterval(function() {
     deg = deg + 1;
-    body_screen.style.background = 'linear-gradient(' + deg%360 + 'deg, var(--main-blue), rgb(150, 240, 220))';
+    body_screen.style.background = 'linear-gradient(' + deg%360 + 'deg, var(--main-blue-40), rgba(150, 240, 220,0.4))';
 }, 30);
