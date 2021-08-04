5e50b1c40d21028b8cfbba9a9f2c0d2591823896
jomakerforGod <74284597+joseph0305@users.noreply.github.com>
2021-08-04 18:21:39 +0900

    Modified Main page Set-up icon
    설정 창을 눌렀을때 옆에서 사이드바가 나오도록 만들었습니다. 이때 다른 부분에도 라벨을 추가해서 다른 영역을 클릭했을때 사이드바가 닫도록 만들었습니다.

--- public/javascripts/Main.js
+++ public/javascripts/Main.js
@@ -32,12 +32,13 @@
 //});
 
 // host -> 현재 창의 주소를 담고 있는 변수.
-var host = window.location.protocol + "//" + window.location.host;
+//이거 다시 회복
+/* var host = window.location.protocol + "//" + window.location.host;
 sendAjax(host + '/main/data', "GET", function(Data){
     Makehtml(Data);
-});
-
-
+}); */
+// 이거 다시 회복
+/* 
 var SavedGetData; 
 function sendAjax(url, method, call) {
 	const xhr = new XMLHttpRequest();
@@ -51,7 +52,7 @@ function sendAjax(url, method, call) {
 		console.log("Getting data success!", result);
 		call(result);
     });
-};
+}; */
 
 
 /* let SavedGetData = data; */
@@ -186,12 +187,12 @@ function TS(){
     console.log("hihihih")
 }
 
-const COG = document.querySelector("#Picture");
-COG.addEventListener("click",Sidebar)
-function Sidebar(){
-    /* document.querySelector("#sidebar").style.left = "0px"; */
-    document.querySelector("#sidebar").classList.toggle("to")
-}
+
+
+
+
+
+
 
 
 
