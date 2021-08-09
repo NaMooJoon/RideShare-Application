e86317ebdd3035f720954e4c04c2d6809b87ab96
yjun02 <jyjoons@gmail.com>
2021-08-10 02:27:56 +0900

    Profile page's design totally changed

--- public/javascripts/Profile.js
+++ public/javascripts/Profile.js
@@ -14,7 +14,6 @@ function StopAndSave(event){
 }
 
 
-
 const SavedUsername = localStorage.getItem("username");
 const SavedUserinfo = localStorage.getItem("userinfo");
 
@@ -29,12 +28,34 @@ else{
 }
 
 function MovePage(){
+    
     window.location.href="../../views/Main.html"
 }
 BT.addEventListener("click",MovePage);
 //버튼 눌렀을시 다른 screen으로 가는 기능
 //if onclik
 
+const display = document.getElementById("canvas_display");
+const canvas = document.getElementById("profile_canvas");
 
-
-
+function handleImageView(files){		
+	var file = files[0];
+	if(!file.type.match(/image.*/)){
+		alert("not image file!");
+        display.src="../public/images/profile_null.png"
+	}			
+	var reader = new FileReader();
+	
+	reader.onload = function(e){
+		var img = new Image();
+		img.onload = function(){
+			var ctx = document.getElementById("profile_canvas").getContext("2d");
+            ctx.fillStyle = 'white'
+            ctx.fillRect(0,0,250,250);
+			ctx.drawImage(img,0,0,250,250);
+		}
+		img.src = e.target.result;
+        display.src = e.target.result;
+	}
+	reader.readAsDataURL(file);
+}
