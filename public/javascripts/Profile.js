33c6c6351d8cb90603426215f0e8f7d4e5e3251d
yjun02 <jyjoons@gmail.com>
2021-08-10 04:21:39 +0900

    Developed primary image transfer function

--- public/javascripts/Profile.js
+++ public/javascripts/Profile.js
@@ -22,21 +22,43 @@ if (SavedUsername === null && SavedUserinfo===null){
     UserForm.addEventListener("submit",StopAndSave)
 }
 else{
-    window.location.href="../../views/Main.html" // 이 페이지 뛰어넘는 코드
+    //window.location.href="../views/Main.html" // 이 페이지 뛰어넘는 코드
     /*  After.classList.remove("hidden"); */ // 신경식 유저님 환영합니다
-                                      
+                                
 }
 
-function MovePage(){
+
+function saveImage() {
+    var $canvas = document.createElement('canvas');
+    var imgDataUrl = $canvas.toDataURL('image/png');
     
-    window.location.href="../../views/Main.html"
+    var blobBin = atob(imgDataUrl.split(',')[1]);	// base64 데이터 디코딩
+    var array = [];
+    for (var i = 0; i < blobBin.length; i++) {
+        array.push(blobBin.charCodeAt(i));
+    }
+    var file = new Blob([new Uint8Array(array)], {type: 'image/png'});	// Blob 생성
+    var formdata = new FormData();	// formData 생성
+    formdata.append("file", file);	// file data 추가
+    $.ajax({
+        type : 'post',
+        url : 'localhost/board',
+        data : formdata,
+        processData : false,	// data 파라미터 강제 string 변환 방지!!
+        contentType : false,	// application/x-www-form-urlencoded; 방지!!
+        success : function (data){}
+    })
+    alert("프로필 정보가 등록되었습니다.");
+    window.location.href="../views/Main.html";
 }
-BT.addEventListener("click",MovePage);
+
+
+BT.addEventListener("click",saveImage);
 //버튼 눌렀을시 다른 screen으로 가는 기능
 //if onclik
 
 const display = document.getElementById("canvas_display");
-const canvas = document.getElementById("profile_canvas");
+const canvas = document.getElementById("canvas");
 
 function handleImageView(files){		
 	var file = files[0];
@@ -49,7 +71,7 @@ function handleImageView(files){
 	reader.onload = function(e){
 		var img = new Image();
 		img.onload = function(){
-			var ctx = document.getElementById("profile_canvas").getContext("2d");
+			var ctx = document.getElementById("canvas").getContext("2d");
             ctx.fillStyle = 'white'
             ctx.fillRect(0,0,250,250);
 			ctx.drawImage(img,0,0,250,250);
