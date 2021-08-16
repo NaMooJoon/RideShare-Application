844a4828eb88f1dba189a44e06135ebd8b0f8394
yjun02 <jyjoons@gmail.com>
2021-08-16 12:00:36 +0900

    Developded image upload system

--- public/javascripts/Profile.js
+++ public/javascripts/Profile.js
@@ -3,8 +3,9 @@ const UserForm = document.querySelector("#formTAG");
 const UsernameInput = document.querySelector("#input1");
 const UserInfoInput = document.querySelector("#input2");
 const WHOLE = document.querySelector('#Whole');
-const After = document.querySelector('#after')
-const BT = document.querySelector('#Button')
+const After = document.querySelector('#after');
+const BT = document.querySelector('#Button');
+const display = document.querySelector('#profile_display');
 //
 function StopAndSave(event){
     event.preventDefault();
@@ -33,10 +34,8 @@ var host = window.location.protocol + "//" + window.location.host;
 function sendAjax(url, method, call) {
 	const xhr = new XMLHttpRequest();
 	xhr.open(method, url);
-
 	var data = null;
     xhr.send(data);
-
     xhr.addEventListener('load', function(){
         const result = JSON.parse(xhr.responseText);
 		console.log("Getting data success!", result);
@@ -44,89 +43,42 @@ function sendAjax(url, method, call) {
     });
 };
 
+
+document.getElementById('inputfile').addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    new Compressor(file, options);
+    })
+
+
 sendAjax( host + '/profile/user' , "POST", function(users){
     console.log(users);
 	UsernameInput.innerHTML = users[0].name;
     UserInfoInput.innerHTML = users[0].stID;
-
     BT.addEventListener("click", e => {
         e.preventDefault();
-        saveImage(users[0].stID);
+        saveImage(users[0].stID, new File([result], result.name, { type: result.type }));
     });
 });
 
-function blobToFile(theBlob, fileName){
-    //A Blob() is almost a File() - it's just missing the two properties below which we will add
-    theBlob.lastModifiedDate = new Date();
-    theBlob.name = fileName;
-    return theBlob;
-}
+const options = { maxWidth: 500, maxHeight: 500, success: function (result) { if (result.size > 5*1024*1024) {
+    // 리사이징 했는데도 용량이 큰 경우
+    alert("파일 용량이 초과되어 업로드가 불가 합니다."); return; } 
+    const _URL = window.URL || window.webkitURL;
+    if (_URL) {display.src = _URL.createObjectURL(result)} 
+  }, error: function (err) { console.log(err)}}
 
-
-function saveImage(stID) {
-    var $canvas = document.createElement('canvas');
-    var imgDataUrl = $canvas.toDataURL('image/png');
-    
-    var blobBin = atob(imgDataUrl.split(',')[1]);	// base64 데이터 디코딩
-    var array = [];
-    for (var i = 0; i < blobBin.length; i++) {
-        array.push(blobBin.charCodeAt(i));
-    }
-    var file = new Blob([new Uint8Array(array)], {type: 'image/png'});	// Blob 생성
+function saveImage(stID, file){
+    console.log("저장된 이미지",file)
     var formdata = new FormData();	// formData 생성
-    console.log(file);
-    formdata.append("file", file, `${stID}`);	// file data 추가
-    var png = blobToFile(file, 'test.png');
-    console.log(png);
-
-    console.log("폼데이터 @~@", formdata.get("file"));
-    // const test = document.querySelector("#testimg");
-    // test.src = formdata;
-    // console.dir(test);
-
+    formdata.append("file", file, `${stID}`+'.png');
     $.ajax({
         type : 'post',
-        url : '/profile',
+        url : '../public/images/profile',
         data : formdata,
-        //data: png,
         processData : false,	// data 파라미터 강제 string 변환 방지!!
         contentType : false,	// application/x-www-form-urlencoded; 방지!!
         success : function (data){
             alert("프로필 정보가 등록되었습니다.");
-            window.location.href="/main";
         }
     })
-    
 }
-
-
-
-
-//버튼 눌렀을시 다른 screen으로 가는 기능
-//if onclik
-
-const display = document.getElementById("canvas_display");
-const canvas = document.getElementById("canvas");
-
-function handleImageView(files){		
-	var file = files[0];
-	if(!file.type.match(/image.*/)){
-		alert("not image file!");
-        display.src="../public/images/profile_null.png"
-	}			
-	var reader = new FileReader();
-	
-	reader.onload = function(e){
-		var img = new Image();
-		img.onload = function(){
-			var ctx = document.getElementById("canvas").getContext("2d");
-            ctx.fillStyle = 'white'
-            ctx.fillRect(0,0,250,250);
-			ctx.drawImage(img,0,0,250,250);
-		}
-		img.src = e.target.result;
-        display.src = e.target.result;
-	}
-	reader.readAsDataURL(file);
-}
-
