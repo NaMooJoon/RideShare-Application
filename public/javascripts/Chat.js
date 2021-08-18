0af5a321fb251ad4978d26300fef3de3f0835a51
yjun02 <jyjoons@gmail.com>
2021-08-18 12:54:36 +0900

    Chat page's design improved

--- public/javascripts/Chat.js
+++ public/javascripts/Chat.js
@@ -171,4 +171,10 @@ $(function () {
     });
 });
 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
 
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
